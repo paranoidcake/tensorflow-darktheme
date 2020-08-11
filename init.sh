ac655fcee555b8989ebe4276540ec3c540522b01
paranoidcake <victorpanayotov@gmail.com>
2020-08-11 17:03:32 +0100

    Add init.sh

---
+++ init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+docker build -t tensorflow-darktheme https://github.com/paranoidcake/tensorflow-darktheme.git#master:docker
+
+: ${TFALIAS:="tfgpu"}
+
+runcmd='docker run --gpus all -it -p 8888:8888 -v $PWD:/tf tensorflow/darktheme'
+
+case "$SHELL" in
+    *zsh*)
+        echo "" >> $HOME/.zshrc
+        echo "### Added by tensorflow-darktheme ###" >> $HOME/.zshrc
+        echo "alias $TFALIAS=$runcmd" >> $HOME/.zshrc
+        echo "### ----------------------------- ###" >> $HOME/.zshrc
+        ;;
+    *bash*)
+        echo "" >> $HOME/.bashrc
+        echo "### Added by tensorflow-darktheme ###" >> $HOME/.bashrc
+        echo "alias $TFALIAS=$runcmd" >> $HOME/.bashrc
+        echo "### ----------------------------- ###" >> $HOME/.bashrc
