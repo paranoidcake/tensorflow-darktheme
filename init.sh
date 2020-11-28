8b6d2cd6b550218ec0d99909b76855295625454e
Victor Panayotov <victorpanayotov@gmail.com>
2020-11-28 20:49:26 +0000

    Pass uid and guid to notebook
     - Clean up Dockerfile a little

--- init.sh
+++ init.sh
@@ -3,19 +3,19 @@ docker build -t tensorflow-darktheme https://github.com/paranoidcake/tensorflow-
 
 : ${TFALIAS:="tfgpu"}
 
-runcmd='docker run --gpus all -it -p 8888:8888 -p 6006:6006 -v $PWD:/tf tensorflow-darktheme'
+runcmd='docker run -u $(id -u):$(id -g) --gpus all -it -p 8888:8888 -p 6006:6006 -v $(pwd):/tf tensorflow-darktheme'
 
 case "$SHELL" in
     *zsh*)
         echo "" >> $HOME/.zshrc
         echo "# Lines added by tensorflow-darktheme #" >> $HOME/.zshrc
-        echo "alias $TFALIAS=\'$runcmd\'" >> $HOME/.zshrc
+        echo "alias $TFALIAS='$runcmd'" >> $HOME/.zshrc
         echo "# ----------------------------------- #" >> $HOME/.zshrc
         ;;
     *bash*)
         echo "" >> $HOME/.bashrc
         echo "# Lines added by tensorflow-darktheme #" >> $HOME/.bashrc
-        echo "alias $TFALIAS=\'$runcmd\'" >> $HOME/.bashrc
+        echo "alias $TFALIAS='$runcmd'" >> $HOME/.bashrc
         echo "# ----------------------------------- #" >> $HOME/.bashrc
         ;;
 esac
